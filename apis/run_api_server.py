d6767c9dac0af781671186b20a53be94ea06c897
Johannes E. M. Mosig <jem-mosig@protonmail.com>
2020-11-10 09:59:32 +0100

    Add API Server script
    commit 1cfd1cc3455010090b2b0e2e972191573adba179
    Author: Johannes E. M. Mosig <jem-mosig@protonmail.com>
    Date:   Mon Nov 9 18:08:11 2020 +0100
    
        Use POST
    
    commit 8a37043e14672680c24ff8149fb19ab6ea58a81d
    Author: Johannes E. M. Mosig <jem-mosig@protonmail.com>
    Date:   Mon Nov 9 17:50:33 2020 +0100
    
        Use POST for retrieve
    
    commit 019b3a3c0c0d40fdf78f51492df6a331d7e7e13b
    Author: Johannes E. M. Mosig <jem-mosig@protonmail.com>
    Date:   Thu Nov 5 15:36:34 2020 +0100
    
        Draft API server script

---
+++ apis/run_api_server.py
@@ -0,0 +1,84 @@
+
+from collections import defaultdict
+from typing import Any, Dict, List, Text
+
+from flask import Flask, json, jsonify, request
+
+import api
+
+flask = Flask(__name__)
+
+MOCK_DATA_HEAP = defaultdict(list)
+
+Constraint = List[Dict[Text, Any]]
+
+
+class InvalidUsage(Exception):
+  def __init__(self, message, status_code=None):
+    Exception.__init__(self)
+    self.message = message
+    self.status_code = status_code or 400
+
+  def to_dict(self):
+    return {"Message": self.message}
+
+
+@flask.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+  response = jsonify(error.to_dict())
+  response.status_code = error.status_code
+  return response
+
+
+def convert_comparators(constraints: List[Constraint]) -> List[Constraint]:
+  converted_constraints = []
+  for constraint in constraints:
+    slot = constraint.get("Slot")
+    value = constraint.get("Value")
+    comparator = constraint.get("Comparator")
+    if not comparator:
+      converted_constraints.append({slot: value})
+    else:
+      converted_constraints.append({slot: eval(f"api.{comparator}({value})")})
+  return converted_constraints
+
+
+@flask.route("/<task>", methods=["POST"])
+def retrieve(task):
+  if MOCK_DATA_HEAP.get(task):
+    return MOCK_DATA_HEAP[task].pop(0)
+
+  try:
+    constraints = request.get_json(silent=True)
+    constraints = convert_comparators(constraints)
+  except Exception as e:
+    raise InvalidUsage("Bad constraints string")
+
+  try:
+    item, num_other_items = api.call_api(
+      task, 
+      constraints=constraints,
+    )
+    return jsonify({"Message": "Ok", "Item": item, "NumOtherItems": num_other_items})
+  except Exception as e:
+    raise InvalidUsage("API code failed")
+
+
+@flask.route("/mock/<task>", methods=["POST"])
+def mock(task):
+  """Add responses to the MOCK_DATA_HEAP"""
+
+  MOCK_DATA_HEAP[task].append(request.get_json(silent=True))
+  return ""
+
+
+@flask.route("/mock/clear", methods=["GET"])
+def mock_clear():
+  """Clear MOCK_DATA_HEAP"""
+
+  MOCK_DATA_HEAP.clear()
+  return ""
+
+
+if __name__ == '__main__':
+    flask.run()
